2f6557cbcb323f4d8e3bb4a46450751ddd8e2a33
AceSoL-ls <eyris97@gmail.com>
2026-06-15 22:03:08 +0300

    Added Pictures and the src code

---
+++ React-app-1/src/App.js
@@ -0,0 +1,37 @@
+import PlayerCard from './PlayerCard';
+import nickPic from './assets/Nick.jpg';
+import mariaPic from './assets/Maria.jpg';
+import johnPic from './assets/John.png';
+
+function App(){
+
+ return(
+  <div>
+   <PlayerCard 
+   name = "Nick" 
+   game = "Lineage" 
+   level = "85" 
+   gold = "1000" 
+   image = {nickPic} 
+   />
+   <PlayerCard 
+   name = "Maria" 
+   game = "Valorant" 
+   level = "40" 
+   gold = "500"
+   image = {mariaPic}
+    
+   />
+   <PlayerCard 
+   name ="John" 
+   game = "Minecraft" 
+   level = "100" 
+   gold = "2000"
+   image = {johnPic}
+    
+   />
+  </div>
+ )
+}
+
+export default App;
