8ed74c80197fe33ad94a6ea082a299ba5c9b2c54
AceSoL-ls <eyris97@gmail.com>
2026-06-17 19:33:36 +0300

    Update 2.0 , Massive App.jsx Update using .map() JS Function for data scaling

--- React-app-1/src/App.jsx
+++ React-app-1/src/App.jsx
@@ -6,36 +6,53 @@ import nickPic from './assets/Nick.jpg';
 import mariaPic from './assets/Maria.jpg';
 import johnPic from './assets/John.png';
 
+const PLAYERS_DATA = [
+  {
+    id: 1,
+    name: "Nick",
+    game: "Lineage",
+    level: "85",
+    gold: "1000",
+    image: nickPic
+  },
+  {
+    id: 2,
+    name: "Maria",
+    game: "Valorant",
+    level: "40",
+    gold: "500",
+    image: mariaPic
+  },
+  {
+    id: 3,
+    name: "John",
+    game: "Minecraft",
+    level: "100",
+    gold: "2000",
+    image: johnPic
+  }
+];
+
 function App(){
 
  return(
   <div className="dashboard-container">
    <h1 className="dashboard-title">Leaderboard Lab ⚔️</h1>
+
    <div className="player-grid">
-   <PlayerCard 
-   name = "Nick" 
-   game = "Lineage" 
-   level = "85" 
-   gold = "1000" 
-   image = {nickPic} 
-   />
-   <PlayerCard 
-   name = "Maria" 
-   game = "Valorant" 
-   level = "40" 
-   gold = "500"
-   image = {mariaPic}
-   />
-   <PlayerCard 
-   name ="John" 
-   game = "Minecraft" 
-   level = "100" 
-   gold = "2000"
-   image = {johnPic}
-   />
+      {PLAYERS_DATA.map((player) => (
+         <PlayerCard
+            key={player.id}
+            name={player.name}
+            game={player.game}
+            level={player.level}
+            gold={player.gold}
+            image={player.image}
+         />
+      ))}
    </div>
-  </div>
- )
+   </div>
+ );
 }
 
 export default App;
